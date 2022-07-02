eb69d0e6678ff8778b34d27645c2c675aca74974
udaykiran-i <udaykiranirukula@gmail.com>
2022-07-03 00:40:14 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Design from "./components/Design-1/design";
+import Design2 from "./components/Design-2/design2";
+import Design3 from "./components/Design-3/design3";
+import Menu from "./components/Menu/menu";
+import SpecialMenu from "./components/SpecialMenu/specialMenu";
+
+
+function App() {
+  return (
+    <div>
+      <Design />
+      <Design2 />
+      <Design3 />
+      <Menu />
+      <SpecialMenu />
+    </div>
+  );
+}
+
+
+export default App;
