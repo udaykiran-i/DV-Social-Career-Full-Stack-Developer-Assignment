cab9837646d133c6b3d9fa13814a057c7355c99d
udaykiran-i <udaykiranirukula@gmail.com>
2022-07-03 12:07:04 +0530

    added login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
+import Login from "./components/Login/login";
 import Design from "./components/Design-1/design";
 import Design2 from "./components/Design-2/design2";
 import Design3 from "./components/Design-3/design3";
 import Menu from "./components/Menu/menu";
 import SpecialMenu from "./components/SpecialMenu/specialMenu";
 
-
 function App() {
   return (
     <div>
+      <Login />
       <Design />
       <Design2 />
       <Design3 />
